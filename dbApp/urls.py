551480da0163e80f6585efeba59496263ee20137
Yealim Han <dpfla0376@hanmail.net>
2017-04-02 16:00:32 +0900

    main page & url changed

--- dbApp/urls.py
+++ dbApp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.asset_total, name='asset_total'),
+    url(r'^$', views.service_resources, name='service_resources'),
     url(r'asset$', views.asset_total, name='asset_total'),
 ]
--- dbApp/views.py
+++ dbApp/views.py
@@ -7,3 +7,6 @@ def asset_total(request):
     asset_total_list = Asset.objects.all()
     context = {'asset_total_list': asset_total_list}
     return render(request, 'dbApp/asset_total.html', context)
+
+def service_resources(request):
+    return render(request, 'dbApp/service_resources.html', {});
