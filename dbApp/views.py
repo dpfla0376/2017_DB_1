358d13ddc19e098f1d89b268e2e68b622bee6617
SeHyeon Yang <SeHyeon Yang>
2017-04-02 16:05:23 +0900

    views

--- dbApp/views.py
+++ dbApp/views.py
@@ -8,12 +8,12 @@ def asset_total(request):
     context = {'asset_total_list': asset_total_list}
     return render(request, 'dbApp/asset_total.html', context)
 
-<<<<<<< HEAD
 def server_asset(request):
     server_asset_list = Asset.objects.all()
     context = {'server_asset_list': server_asset_list}
     return render(request, 'dbApp/server_asset.html', context)
-=======
+
 def service_resources(request):
     return render(request, 'dbApp/service_resources.html', {});
->>>>>>> 286f7cdb78193ed30933aef15a08097916dd214b
+
+
